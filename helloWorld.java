b61f26cc1639643b50b4c8569b11f4b02c375443
Dylan Bakr <dylan_bakr@comcast.net>
2020-06-26 14:58:25 -0500

    functions
    added dummy function for welcome menu and ticket entry

--- helloWorld.java
+++ helloWorld.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
 import java.util.IO;
 
-public Class Main{
+public class Main{
+  public static void main(String[] args){
+    Scanner cin = new Scanner(System.in);
+    char user=' ';
+    String ticket=" ";
+    System.out.println("Hello World! Let's fix the airports");
+    welcome();
+    user=cin.nextChar();
+    if(user=='x'){System.out.println("Enter your ticket number: ");}
+    ticket = cin.nextLine();
+    System.out.print("You entered: " + ticket);
+  }
+}
+
+public static welcome(){
+  System.out.println("(x)\tEnter Ticket Number");
+  //System.out.println("(x)\t second menu option");
 }
