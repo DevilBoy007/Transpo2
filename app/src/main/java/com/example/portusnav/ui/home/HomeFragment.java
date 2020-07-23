45f082ab09998cc9d10da9f301604add58b565c9
Aicmille <48917477+Aicmille@users.noreply.github.com>
2020-07-23 12:10:45 -0400

    test

--- app/src/main/java/com/example/portusnav/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/portusnav/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.portusnav.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,23 +14,49 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.portusnav.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Tag;
 
 public class HomeFragment extends Fragment {
 
+
+    DatabaseReference connectedRef = FirebaseDatabase.getInstance().getReference(".info/connected");
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("/tickets");
+
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+    public View onCreateView( LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean connected = snapshot.getValue(Boolean.class);
+                if (connected) {
+                    Log.d(getTag(), "connected");
+                } else {
+                    Log.d(getTag(), "not connected");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(getTag(), "Listener was cancelled");
+            }
+        });
+        myRef.setValue("Hello, World!");
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
+
+
     }
+
+
+
 }
