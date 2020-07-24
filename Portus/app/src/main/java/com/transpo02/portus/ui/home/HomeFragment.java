170a56334a27108cd5ed468120d65586fdcb1dcf
Dylan Bakr <dylan_bakr@comcast.net>
2020-07-24 17:07:12 -0500

    Update HomeFragment.java
    added firebase connection and creating ticket object to store variables to

--- Portus/app/src/main/java/com/transpo02/portus/ui/home/HomeFragment.java
+++ Portus/app/src/main/java/com/transpo02/portus/ui/home/HomeFragment.java
@@ -1,38 +1,48 @@
-package com.transpo02.portus.ui.home;
+package com.example.portusnav.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
-import com.transpo02.portus.R;
+import com.example.portusnav.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeFragment extends Fragment {
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference connectedRef = database.getReference("/tickets");
+    //DatabaseReference myRef = database.getInstance().getReference("/tickets");
+
     private HomeViewModel homeViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView( LayoutInflater inflater,
+                              ViewGroup container, Bundle savedInstanceState) {
+        connectedRef.addValueEventListener(new ValueEventListener() {
+            
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                Ticket ticket = dataSnapshot.getValue(Ticket.class);
+                System.out.println("Hello "+ticket.fname+", in order to make your flight at "+ticket.time+" from "+ticket.dept+", you should arrive 45 minutes before boarding");
+            }
+            
+            @Override
+            public void onCancelled(DatabaseError error) {
+                Log.w(getTag(), "Listener was cancelled");
+            }
+        });
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-            }
-        });
-
         return root;
     }
-
 }
-
